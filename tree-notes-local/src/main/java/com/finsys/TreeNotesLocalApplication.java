e79fce39592dd1939ed6465595e1ea8e0601b478
krisso86 <krisso86@users.noreply.github.com>
2015-07-27 20:41:15 +0200

    first commit

---
+++ tree-notes-local/src/main/java/com/finsys/TreeNotesLocalApplication.java
@@ -0,0 +1,12 @@
+package com.finsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeNotesLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeNotesLocalApplication.class, args);
+    }
+}
